72615683ae0fec97f2b53f07107c0890daeb6ff7
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-11-14 09:27:06 +0200

    Update hero-detail.component.ts

--- application/marvel-like/src/app/component/hero-detail/hero-detail.component.ts
+++ application/marvel-like/src/app/component/hero-detail/hero-detail.component.ts
@@ -25,6 +25,7 @@ export class HeroDetailComponent implements OnInit {
     this.getHero()
   }
 
+  
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
